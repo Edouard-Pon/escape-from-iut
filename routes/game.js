cc77b320bac3f642a1d46860f88535b3e2dbd5dc
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-02-16 00:42:08 +0100

    Added hints

--- routes/game.js
+++ routes/game.js
@@ -10,7 +10,37 @@ router.get('/', (req, res) => {
     }
 })
 
-router.post('/', async (req, res) => {
+router.get('/chapter1', (req, res) => {
+    res.render('game/chapter1')
+})
+
+router.get('/chapter2', (req, res) => {
+    if (req.cookies['You_Ended_The_Game!'] !== undefined) {
+        const finalCode = req.cookies['something'].replace(/[;]/g, '')
+        res.render('game/chapter2', { valideCode: true, finalCode: finalCode })
+    } else {
+        res.render('game/chapter2', { valideCode: false, finalCode: '' })
+    }
+})
+
+router.get('/chapter3', (req, res) => {
+    if (req.cookies['You_Ended_The_Game!'] !== undefined) {
+        const finalCode = req.cookies['something'].replace(/[;]/g, '')
+        res.render('game/chapter3', { finalCode: finalCode })
+    } else {
+        res.render('game/chapter3', { finalCode: '' })
+    }
+})
+
+router.get('/chapter4', (req, res) => {
+    res.render('game/chapter4')
+})
+
+router.get('/end', (req, res) => {
+    res.render('game/end')
+})
+
+router.post('/chapter2', async (req, res) => {
     try {
         if (req.cookies['something'] !== undefined) {
             const finalCode = req.body.passwd
@@ -23,10 +53,10 @@ router.post('/', async (req, res) => {
                 }
 
                 res.cookie('You_Ended_The_Game!', 'Congrats!', options)
-                res.render('game/index', { valideCode: true, finalCode: actualCode })
+                res.render('game/chapter2', { valideCode: true, finalCode: actualCode })
             }
         } else {
-            res.render('game/index', { valideCode: false, finalCode: '' })
+            res.render('game/chapter2', { valideCode: false, finalCode: '' })
         }
     } catch (err) {
         console.log(err)
